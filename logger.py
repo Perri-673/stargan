19deb51c555dce82cf4a040997f6582a7214eebb
Perri-673 <110961097+Perri-673@users.noreply.github.com>
2025-01-29 12:47:38 +0900

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 
-
 class Logger(object):
     """Tensorboard logger."""
 
@@ -10,5 +9,6 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Add scalar summary."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
